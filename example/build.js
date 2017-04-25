6664fc2a7b3f8623c41e7c2fc8dee3b176da57c1
xiaoyann <0x0886@gmail.com>
2017-04-25 11:23:09 +0800

    docs

---
+++ example/build.js
@@ -0,0 +1,112 @@
+var path = require('path')
+var webpack = require('webpack')
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+var ExtractTextPlugin = require('extract-text-webpack-plugin')
+
+var config = {
+
+  context: __dirname,
+
+  devtool: 'source-map',
+  
+  entry: {
+    app: [
+      './main.js'
+    ],
+    vendor: [
+      'vue', 
+      'vue-router'
+    ]
+  },
+
+  output: {
+    filename: 'js/[name].[chunkhash].js',
+    path: path.resolve(__dirname, '../docs')
+  },
+
+  resolve: {
+    extensions: ['.js', '.vue'],
+  },
+
+  module: {
+    rules: [
+      {
+        test: /.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+        loader: 'url-loader'
+      },
+      {
+        test: /\.css$/,
+        loaders: ['style-loader', 'css-loader'] 
+      },
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+        options: {
+          loaders: {
+            stylus: ExtractTextPlugin.extract({
+              loader: 'css-loader?minimize!stylus-loader?sourceMap'
+            })
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract('css-loader?minimize&sourceMap')
+      },
+      {
+        test: /\.(styl)$/,
+        loader: ExtractTextPlugin.extract(['css-loader?minimize&sourceMap', 'stylus-loader'])
+      }
+    ]
+  },
+
+  plugins: [
+    new webpack.DefinePlugin({
+      'process.env.NODE_ENV': '"production"'
+    }),
+
+    // uglify js
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false
+      },
+      sourceMap: true,
+      minimize: true
+    }),
+
+    new webpack.optimize.CommonsChunkPlugin({
+      names: ['vendor']
+    }),
+
+    // https://github.com/ampedandwired/html-webpack-plugin
+    new HtmlWebpackPlugin({
+      template: './index.html'
+    }),
+
+    // extract css into its own file
+    new ExtractTextPlugin('css/[name].[contenthash].css'),
+  ],
+
+  performance: {
+    hints: process.env.NODE_ENV === 'production'
+  },
+}
+
+webpack(config, function(err, stats) {
+  if (err) throw err
+  console.log(stats.toString({
+    colors: true,
+    chunks: false,
+    children: false
+  }))
+})
+
+
+
+
+
