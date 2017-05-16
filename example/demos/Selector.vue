16250fdd06b0842e75d0ea0e0932f91ce7d0217d
xiaoyann <0x0886@gmail.com>
2017-05-16 19:05:28 +0800

    adjust the code style

--- example/demos/Selector.vue
+++ example/demos/Selector.vue
@@ -1,34 +1,34 @@
 <template>
 <div>
-	<ul>
-		<li>在哪啊：{{city}}</li>
-	</ul>
-	<Cell title="在哪啊" arrow>
-		<Selector slot="body" v-model="city" placeholder="请选择">
-			<option :value="item.val" v-for="item in options">{{item.text}}</option>
-		</Selector>
-	</Cell>
-</div>	
+  <ul>
+    <li>在哪啊：{{city}}</li>
+  </ul>
+  <Cell title="在哪啊" arrow>
+    <Selector slot="body" v-model="city" placeholder="请选择">
+      <option :value="item.val" v-for="item in options">{{item.text}}</option>
+    </Selector>
+  </Cell>
+</div>
 </template>
 
 <script>
 export default {
-	data() {
-		return {
-			city: "",
-			options: []
-		}
-	},
+  data() {
+    return {
+      city: "",
+      options: []
+    }
+  },
 
-	mounted() {
-		setTimeout(() => {
-			this.options = [
-				{ text: '北京', val: 1 },
-				{ text: '上海', val: 2 },
-				{ text: '广州', val: 3 },
-				{ text: '深圳', val: 4 }
-			]
-		}, 100)
-	}
+  mounted() {
+    setTimeout(() => {
+      this.options = [
+        { text: '北京', val: 1 },
+        { text: '上海', val: 2 },
+        { text: '广州', val: 3 },
+        { text: '深圳', val: 4 }
+      ]
+    }, 100)
+  }
 }
 </script>
