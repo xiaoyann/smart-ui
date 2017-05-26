d98585ad81e93d1cfc44985ee3ef842ebdc4f0f6
xiaoyann <0x0886@gmail.com>
2017-05-26 23:55:35 +0800

    docs(docs): update docs for Selector

--- docs/docs/src/pages/selector/index.js
+++ docs/docs/src/pages/selector/index.js
@@ -1,4 +1,49 @@
 import create from './index.tpl'
-export default create({
 
+const source = {}
+
+source.html = `
+<div>
+  <ul>
+    <li>在哪啊：{{city}}</li>
+  </ul>
+  <Cell title="在哪啊" arrow>
+    <Selector slot="body" v-model="city">
+      <option :value="item.val" v-for="item in options">{{item.text}}</option>
+    </Selector>
+  </Cell>
+</div>
+`
+
+source.javascript = `
+import create from './index.tpl'
+
+export default create({
+  data() {
+    return {
+      city: "",
+      options: [
+        { text: '北京', val: 1 },
+        { text: '上海', val: 2 },
+        { text: '广州', val: 3 },
+        { text: '深圳', val: 4 }
+      ]
+    }
+  }
+})
+`
+
+export default create({
+  data() {
+    return {
+      city: "",
+      options: [
+        { text: '北京', val: 1 },
+        { text: '上海', val: 2 },
+        { text: '广州', val: 3 },
+        { text: '深圳', val: 4 }
+      ],
+      source
+    }
+  }
 })
--- docs/docs/src/pages/selector/index.tpl
+++ docs/docs/src/pages/selector/index.tpl
@@ -1,3 +1,19 @@
 <div>
-<h2>Popup</h2>
+<h2>Selector</h2>
+<p>
+  还是调用的原生的控件
+</p>
+
+<smart-demo :source="source">
+<div slot="demo">
+  <ul>
+    <li>在哪啊：{{city}}</li>
+  </ul>
+  <Cell title="在哪啊" arrow>
+    <Selector slot="body" v-model="city">
+      <option :value="item.val" v-for="item in options">{{item.text}}</option>
+    </Selector>
+  </Cell>
+</div>
+</smart-demo>
 </div>
