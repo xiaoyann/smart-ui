fcf324097fa2b83bc01c62700b482105542e2c57
xiaoyann <0x0886@gmail.com>
2017-06-10 15:42:14 +0800

    docs(docs): update docs for Loadmore

--- docs/docs/src/pages/loadmore/index.js
+++ docs/docs/src/pages/loadmore/index.js
@@ -1,4 +1,39 @@
 import create from './index.tpl'
-export default create({
 
+const htmlcode = `
+<Loadmore @reachBottom="loadmore" :allLoaded="allLoaded"></Loadmore>
+`
+
+const jscode = `
+export default {
+  data() {
+    return {
+      list: [],
+      allLoaded: false
+    }
+  },
+
+  methods: {
+    loadmore() {
+      // 每次页面滚动到底部时开始加载数据
+      // 如果数据加载完成可以 allLoaded 设置为 true
+      fetch(...).then(resp => {
+        if (resp.list.length > 0) {
+          this.list = this.list.concat(resp.list)
+        } else {
+          allLoaded = true
+        }
+      })
+    }
+  }
+}
+`
+
+export default create({
+  data() {
+    return {
+      htmlcode,
+      jscode
+    }
+  }
 })
--- docs/docs/src/pages/loadmore/index.tpl
+++ docs/docs/src/pages/loadmore/index.tpl
@@ -1,3 +1,10 @@
 <div>
-<h2>Popup</h2>
+<h2>Loadmore</h2>
+
+<p>
+将该组件放在页面底部，当页面滚动到底部时，触发 <code>reachBottom</code> 事件，<code>allLoaded</code> 属性表示数据已加载完所有数据，当 <code>allLoaded</code> 为 <code>true</code> 时，页面滚动到底部不再触发事件
+</p>
+
+<smart-code :code="htmlcode" lang="html" />
+<smart-code :code="jscode" lang="js" />
 </div>
